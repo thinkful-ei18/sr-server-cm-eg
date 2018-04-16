c204571a364266fdf95faa26cededaabdbb908f9
Christina Moore <sayhitochristina@gmail.com>
2018-04-16 11:00:04 -0500

    make user schema and import linked list class

--- models/user.js
+++ models/user.js
@@ -2,11 +2,31 @@
 
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const LinkedList = require('../linkedList/linkedlist');
 
 const userSchema = mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   firstName: { type: String, default: '' },
   lastName: { type: String, default: '' },
-  questions: [{ type: String }]
+  questions: [{ type: LinkedList }]
 });
+
+userSchema.set('toObject', {
+  transform: function (doc, ret) {
+    ret.id = ret._id,
+      delete ret._id,
+      delete ret.password,
+      delete ret.__v;
+  }
+});
+
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = function (password) {
+  return bcrypt.hash(password, 10);
+}
+
+module.exports = mongoose.model('User', userSchema);
